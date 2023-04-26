f667e17768978dbf4c51cfd72b99164fcbef649f
Jennifer <jennifer.evandia@qualitydigital.global>
2023-04-26 10:56:26 -0300

    aula 2

---
+++ cypress/e2e/examples/features/Login.feature
@@ -0,0 +1,9 @@
+Feature: Teste Login 
+
+    Scenario: 
+        Given eu acesso a pagina home
+        When acesso o login
+        When insiro o meu nome de usuário
+        When insiro uma senha incorreta
+        When Quando clico no botão
+        Then Então devo ver uma mensagem de erro
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -8,3 +8,26 @@ When("I search for Grupo Quality", () => {
     cy.get('[title="Pesquisar"]').type('Quality Digital{enter}')
 })
 
+Given("eu acesso a pagina home", () => {
+    cy.visit("https://automationexercise.com/");
+})
+
+When("acesso o login", () => {
+    cy.get('.shop-menu > .nav > :nth-child(4)').click()
+})
+
+When("insiro o meu nome de usuário", () => {
+    cy.get('input[data-qa="login-email"]').type('jennifer.evandia@teste.com')
+})
+
+When("insiro uma senha incorreta", () => {
+    cy.get('input[data-qa="login-password"]').type('senha')
+})
+
+ When("Quando clico no botão", () => {
+    cy.get('[data-qa="login-button"]').click()
+})
+
+Then("Então devo ver uma mensagem de erro", () => {
+    cy.get('.login-form > form > p').should('have.text','Your email or password is incorrect!')
+})
